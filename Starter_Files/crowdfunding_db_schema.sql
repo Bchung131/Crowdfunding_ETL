5e264b7d25894683b8fd8b313f78333baa55e0e7
Bchung131 <brian13196@gmail.com>
2024-01-07 12:46:15 -0500

    Uploading erd, and sql files.

---
+++ Starter_Files/crowdfunding_db_schema.sql
@@ -0,0 +1,56 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Category" (
+    "category_id" VARCHAR(4)   NOT NULL,
+    "category" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" VARCHAR(7)   NOT NULL,
+    "subcategory" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Contact" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "email" VARCHAR(60)   NOT NULL,
+    CONSTRAINT "pk_Contact" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(50)   NOT NULL,
+    "description" VARCHAR(100)   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(10)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(2)   NOT NULL,
+    "currency" VARCHAR(3)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(4)   NOT NULL,
+    "subcategory_id" VARCHAR(7)   NOT NULL
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contact" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
